ca1ee8ef27ad13716578887500b055741ce8d3e4
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-15 17:05:42 -0800

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className='relative'>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
