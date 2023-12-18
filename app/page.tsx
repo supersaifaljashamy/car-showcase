73580bb420e17ab894946c3fbd3afa5cfe0d4c49
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-18 14:54:52 -0800

    functionality for fuels and years of production

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
 import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
 
@@ -27,8 +28,8 @@ export default async function Home({ searchParams }: HomeProps) {
           <SearchBar />
 
           <div className='home__filter-container'>
-          {/*  <CustomFilter title='fuel' options={fuels} />
-            <CustomFilter title='year' options={yearsOfProduction} /> */}
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} /> 
           </div>
         </div>
 
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -80,7 +80,7 @@ const SearchBar = () => {
           name='model'
           value={model}
           onChange={(e) => setModel(e.target.value)}
-          placeholder='Tiguan...'
+          placeholder='Tiguan'
           className='searchbar__input'
         />
         <SearchButton otherClasses='sm:hidden' />
