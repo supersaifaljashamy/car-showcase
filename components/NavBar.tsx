5724dca22b7f8f9a8248fc1486a4ad302e63e4c9
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-18 19:52:25 -0800

    stop auto-scrolling when search parameters are updated

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -15,12 +15,7 @@ const NavBar = () => (
           className='object-contain'
         />
       </Link>
-
-      <CustomButton
-        title='Sign in'
-        btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-      />
+      
     </nav>
   </header>
 );
